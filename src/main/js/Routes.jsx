893503c869718b890c2effe5e8381b2afc6decf5
Eric Bartusch <ericbartusch8@gmail.com>
2018-05-11 13:09:15 -0500

    Removing executor info stuff

--- src/main/js/Routes.jsx
+++ src/main/js/Routes.jsx
@@ -1,11 +1,9 @@
 import React from 'react';
 import { Route } from 'react-router';
-import { ExecutorInfoPage } from './ExecutorInfoPage';
 import { EnvironmentInfoPage } from './EnvironmentInfoPage';
 
 export default (
     <Route>
-        <Route path="/organizations/:organization/executor-info" component={ExecutorInfoPage} />
         <Route path="/organizations/:organization/:pipeline/environment-info" component={EnvironmentInfoPage} />
     </Route>
 );
