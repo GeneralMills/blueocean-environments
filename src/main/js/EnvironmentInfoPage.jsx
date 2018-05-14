7569c61aaa49254f2fed18750f3eef826ad09f7e
Eric Bartusch <ericbartusch8@gmail.com>
2018-05-14 13:22:22 -0500

    Stylin'

--- src/main/js/EnvironmentInfoPage.jsx
+++ src/main/js/EnvironmentInfoPage.jsx
@@ -1,33 +1,36 @@
 import React from 'react';
 import environmentInfoService from './EnvironmentInfoService';
+import moment from 'moment';
 import { Fetch, UrlConfig } from '@jenkins-cd/blueocean-core-js';
 import { observer } from 'mobx-react';
 
 @observer
 export class EnvironmentInfoPage extends React.Component {
-
     constructor(props) {
         super(props);
 
         this.state = {
-          foundDev:false,
-          foundQA:false,
-          foundProd:false,
+            foundDev:false,
+            foundQA:false,
+            foundProd:false,
 
-          devBranch:"",
-          devRun:"",
-          devCommit:"",
-          devStartTime:"",
+            devBranch:"",
+            devRun:"",
+            devCommit:"",
+            devStartTime:"",
+            devUrl:"",
 
-          qaBranch:"",
-          qaRun:"",
-          qaCommit:"",
-          qaStartTime:"",
+            qaBranch:"",
+            qaRun:"",
+            qaCommit:"",
+            qaStartTime:"",
+            qaUrl:"",
 
-          prodBranch:"",
-          prodRun:"",
-          prodCommit:"",
-          prodStartTime:""
+            prodBranch:"",
+            prodRun:"",
+            prodCommit:"",
+            prodStartTime:"",
+            prodUrl:""
         };
     }
 
@@ -37,6 +40,18 @@ export class EnvironmentInfoPage extends React.Component {
         for(var i = 0; i < nestedPipeline.length; i++) {
             baseUrl = `${baseUrl}/pipelines/${nestedPipeline[i]}`;
         }
+
+        return baseUrl;
+    }
+
+    generatePipelineUrl(organization, pipeline, branch, run) {
+        let baseUrl = `${UrlConfig.getBlueOceanAppURL()}/organizations/${organization}/`;
+        let nestedPipeline = pipeline.split("/");
+        for(var i = 0; i < nestedPipeline.length - 1; i++) {
+            baseUrl = `${baseUrl}${nestedPipeline[i]}%2F`;
+        }
+        baseUrl = `${baseUrl}${nestedPipeline[nestedPipeline.length - 1]}`;
+        baseUrl = `${baseUrl}/detail/${branch}/${run}/pipeline/`;
 
         return baseUrl;
     }
@@ -63,8 +78,9 @@ export class EnvironmentInfoPage extends React.Component {
                         let branchName = response[x].pipeline;
                         let run = response[x].id;
                         let commit = response[x].commitId;
-                        let startTime = response[x].startTime;
+                        let startTime = moment(new Date(response[x].startTime)).format("MM/DD/YYYY HH:mma Z");
                         let stages = pipelines[j];
+                        let pipelineUrl = this.generatePipelineUrl(this.props.params.organization, this.props.params.pipeline, branchName, run);
                         for(var k = 0; k < stages.length; k++) {
                             let stage = stages[k]
                             if(devStages.includes(stage.displayName) && stage.result === "SUCCESS" && stage.state === "FINISHED" && !self.state.foundDev) {
@@ -72,8 +88,9 @@ export class EnvironmentInfoPage extends React.Component {
                                     foundDev: true,
                                     devBranch: branchName,
                                     devRun: run,
-                                    devCommit: commit,
+                                    devCommit: commit.substring(0, 6),
                                     devStartTime: startTime,
+                                    devUrl: pipelineUrl,
                                 });
                             }
                             if(qaStages.includes(stage.displayName) && stage.result === "SUCCESS" && stage.state === "FINISHED" && !self.state.foundQA) {
@@ -81,8 +98,9 @@ export class EnvironmentInfoPage extends React.Component {
                                     foundQA: true,
                                     qaBranch: branchName,
                                     qaRun: run,
-                                    qaCommit: commit,
+                                    qaCommit: commit.substring(0, 6),
                                     qaStartTime: startTime,
+                                    qaUrl: pipelineUrl,
                                 });
                             }
                             if(prodStages.includes(stage.displayName) && stage.result === "SUCCESS" && stage.state === "FINISHED" && !self.state.foundProd) {
@@ -90,8 +108,9 @@ export class EnvironmentInfoPage extends React.Component {
                                     foundProd: true,
                                     prodBranch: branchName,
                                     prodRun: run,
-                                    prodCommit: commit,
+                                    prodCommit: commit.substring(0, 6),
                                     prodStartTime: startTime,
+                                    prodUrl: pipelineUrl,
                                 });
                             }
                         }
@@ -107,24 +126,45 @@ export class EnvironmentInfoPage extends React.Component {
 
     render() {
         return (
-            <div>
-                <div className="container">
-                    <h1>Dev</h1>
-                    <div>{this.state.devBranch} - {this.state.devRun}</div>
-                    <div>{this.state.devCommit}</div>
-                    <div>{this.state.devStartTime}</div>
+            <div className="container">
+                <div>
+                    <div className="header">
+                        <div>Development</div>
+                    </div>
+                    <a href={`${this.state.devUrl}`} target="_blank">
+                        <div className="body">
+                            <div className="branch">{this.state.devBranch} {this.state.devRun}</div>
+                            <div className="time">{this.state.devStartTime}</div>
+                            <div className="commit">commit {this.state.devCommit}</div>
+                            <div className="pipelineText">View Pipeline</div>
+                        </div>
+                    </a>
                 </div>
                 <div>
-                    <h1>QA</h1>
-                    <div>{this.state.qaBranch} - {this.state.qaRun}</div>
-                    <div>{this.state.qaCommit}</div>
-                    <div>{this.state.qaStartTime}</div>
+                    <div className="header">
+                        <div>QA</div>
+                    </div>
+                    <a href={`${this.state.qaUrl}`} target="_blank">
+                        <div className="body">
+                            <div className="branch">{this.state.qaBranch} {this.state.qaRun}</div>
+                            <div className="time">{this.state.qaStartTime}</div>
+                            <div className="commit">commit {this.state.qaCommit}</div>
+                            <div className="pipelineText">View Pipeline</div>
+                        </div>
+                    </a>
                 </div>
                 <div>
-                    <h1>Prod</h1>
-                    <div>{this.state.prodBranch} - {this.state.prodRun}</div>
-                    <div>{this.state.prodCommit}</div>
-                    <div>{this.state.prodStartTime}</div>
+                    <div className="header">
+                        <div>Production</div>
+                    </div>
+                    <a href={`${this.state.prodUrl}`} target="_blank">
+                        <div className="body">
+                            <div className="branch">{this.state.prodBranch} {this.state.prodRun}</div>
+                            <div className="time">{this.state.prodStartTime}</div>
+                            <div className="commit">commit {this.state.prodCommit}</div>
+                            <div className="pipelineText">View Pipeline</div>
+                        </div>
+                    </a>
                 </div>
             </div>
         );
