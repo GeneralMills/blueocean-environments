07157c4e3b4a165fe26c7afd1beb5338303c029d
Erik Elkins <erik.elkins@outlook.com>
2018-06-13 08:50:09 -0500

    Beginning refactor for performance

--- src/main/js/EnvironmentInfoPage.jsx
+++ src/main/js/EnvironmentInfoPage.jsx
@@ -14,6 +14,9 @@ require("babel-polyfill");
 
 @observer
 export class EnvironmentInfoPage extends React.Component {
+
+    stageEnvironments = [];
+
     constructor(props) {
         super(props);
         this.state = {
@@ -34,15 +37,14 @@ export class EnvironmentInfoPage extends React.Component {
     }
 
     async componentWillMount() {
-        let organization = this.props.params.organization;
-        let stageEnvironments = environmentInfoService.stages.split(",");
+        this.stageEnvironments = environmentInfoService.stages.split(",");
         let pipeline = this.props.params.pipeline;
-        let promises = [];
+
         this.setState({
-            activityUrl: `/jenkins/blue/organizations/${organization}/${pipeline}/activity/`,
+            activityUrl: `/jenkins/blue/organizations/${this.props.params.organization}/${pipeline}/activity/`,
         });
         const RestPaths = Paths.rest;
-        const href = RestPaths.pipeline(organization, pipeline);
+        const href = RestPaths.pipeline(this.props.params.organization, pipeline);
         let pipelineResponse = await pipelineService.fetchPipeline(href, { useCache: true, disableCapabilites: false });
         this.setState({
             pipelineObject: pipelineResponse,
@@ -50,7 +52,7 @@ export class EnvironmentInfoPage extends React.Component {
         });
 
         let pipelineRef = {
-            organization: organization,
+            organization: this.props.params.organization,
             pipeline: this.state.pipelineObject,
         };
         let baseUrl = UrlUtils.getRestUrl(pipelineRef);
@@ -58,61 +60,66 @@ export class EnvironmentInfoPage extends React.Component {
         let isMultibranchPipeline = capable(typeResponse, 'io.jenkins.blueocean.rest.model.BlueMultiBranchPipeline');
 
         // Get the pipeline Runs
-        let runResponse = await Fetch.fetchJSON(`${baseUrl}/runs/`);
+        let branchResponse = await Fetch.fetchJSON(`${baseUrl}/branches/`);
         
-        for (let run of runResponse) {
-            // rest api works differently for multibranch pipelines
-            if (isMultibranchPipeline) {
-                promises.push(Fetch.fetchJSON(`${baseUrl}/branches/${run.pipeline}/runs/${run.id}/nodes/`));
-            }
-            else {
-                promises.push(Fetch.fetchJSON(`${baseUrl}/runs/${run.id}/nodes/`));
+        for (let branchJob of branchResponse) {
+            let runResponse;
+            let nodesResponse;
+
+            for (let i = branchJob.latestRun.id; i > 0; i--) {
+                // rest api works differently for multibranch pipelines
+                if (isMultibranchPipeline) {
+                    runResponse = await Fetch.fetchJSON(`${baseUrl}/branches/${branchJob.name}/runs/${i}`);
+                    nodesResponse = await Fetch.fetchJSON(`${baseUrl}/branches/${branchJob.name}/runs/${i}/nodes/`);
+                }
+                else {
+                    runResponse = await Fetch.fetchJSON(`${baseUrl}/runs/${i}/`);
+                    nodesResponse = await Fetch.fetchJSON(`${baseUrl}/runs/${i}/nodes/`);
+                }
+    
+                this.evaluateRunForEnvironments(runResponse, nodesResponse);
             }
         }
 
-        if (runResponse.length === 0) {
+        if (branchResponse.length === 0) {
             this.setState({
                 neverBeenRun: true
             });
         }
 
-        // Get the stages of each run
-        let pipelines = await Promise.all(promises);
-        let x = 0;
-        
-        for (let stages of pipelines) {
-            let branchName = decodeURIComponent(runResponse[x].pipeline);
-            let run = runResponse[x].id;
-            let commit = runResponse[x].commitId;
-            let startTime = moment(new Date(runResponse[x].startTime), "MM/DD/YYYY HH:mma");
-            let now = moment(new Date(), "MM/DD/YYYY HH:mma");
-            let difference = moment.duration(now.diff(startTime)).humanize();
-            
-            let pipelineUrl = this.generatePipelineUrl(organization, pipelineResponse.fullName, encodeURIComponent(branchName), run);
-
-            for (let stage of stages) {
-                if (stageEnvironments.includes(stage.displayName.toLowerCase()) && stage.result === "SUCCESS" && stage.state === "FINISHED") {
-                    let pipelineEnvironmentStage = new PipelineEnvironment(stage.displayName, branchName, run, startTime.format("MM/DD/YYYY HH:mma"), difference, pipelineUrl, commit.substring(0, 6));
-                    
-                    let filteredEnvs = this.state.stagePipelineEnvironments.filter((stagePipelineEnvironment) => 
-                        stagePipelineEnvironment.stageName == stage.displayName
-                    );
-
-                    if (filteredEnvs.length == 0) {
-                        let temp = this.state.stagePipelineEnvironments;
-                        temp.push(pipelineEnvironmentStage);
-
-                        this.setState({
-                            stagePipelineEnvironments: temp
-                        });
-                    }
-                }
-            }
-            x++;
-        }
         this.setState({
             isLoading: false
         });
+    }
+
+    evaluateRunForEnvironments(run, stages) {
+        let branchName = decodeURIComponent(run.name);
+        
+        let commit = run.commitId;
+        let startTime = moment(new Date(run.startTime), "MM/DD/YYYY HH:mma");
+        let now = moment(new Date(), "MM/DD/YYYY HH:mma");
+        let difference = moment.duration(now.diff(startTime)).humanize();
+        
+        let pipelineUrl = this.generatePipelineUrl(this.props.params.organization, run.pipeline, encodeURIComponent(branchName), run.id);
+
+        for (let stage of stages) {
+            if (this.stageEnvironments.includes(stage.displayName.toLowerCase()) && stage.result === "SUCCESS" && stage.state === "FINISHED") {
+                let pipelineEnvironmentStage = new PipelineEnvironment(stage.displayName, branchName, run, startTime.format("MM/DD/YYYY HH:mma"), difference, pipelineUrl, commit.substring(0, 6));
+                
+                let filteredEnvs = this.state.stagePipelineEnvironments.filter((stagePipelineEnvironment) => 
+                    stagePipelineEnvironment.stageName == stage.displayName
+                );
+
+                if (filteredEnvs.length == 0) {
+                    let temp = this.state.stagePipelineEnvironments;
+                    temp.push(pipelineEnvironmentStage);
+
+                    this.setState({
+                        stagePipelineEnvironments: temp
+                    });
+                }
+            }
+        }
     }
 
     pipelineEnvironment(pipelineEnvironment) {
@@ -126,8 +133,8 @@ export class EnvironmentInfoPage extends React.Component {
                     <div className="branchName">{pipelineEnvironment.branch}</div>
                     <div className="branchRun"># {pipelineEnvironment.run}</div>
                 </div>
-                <div className="timeStamp">{pipelineEnvironment.startTime} ({pipelineEnvironment.difference} ago)</div>
-                {pipelineEnvironment.commit && <div className="commitHash">commit {pipelineEnvironment.commit}</div>}
+                <div className="timeStamp"><span className="mega-octicon octicon-clock"></span>{pipelineEnvironment.startTime} ({pipelineEnvironment.difference} ago)</div>
+                {pipelineEnvironment.commit && <div className="commitHash"><span className="mega-octicon octicon-code"></span>commit {pipelineEnvironment.commit}</div>}
                 {pipelineEnvironment.url && <div className="pipelineText">View Pipeline</div>}
             </div>
         </a>
@@ -136,9 +143,9 @@ export class EnvironmentInfoPage extends React.Component {
     render() {
         const pipeline = this.props.params.pipeline;
         const organization = this.props.params.organization;
-        const activityUrl = UrlUtils.buildPipelineUrl(organization, pipeline, 'activity');
-        const branchesUrl = UrlUtils.buildPipelineUrl(organization, pipeline, 'branches');
-        const prUrl = UrlUtils.buildPipelineUrl(organization, pipeline, 'pr');
+        const activityUrl = UrlUtils.buildPipelineUrl(this.props.params.organization, pipeline, 'activity');
+        const branchesUrl = UrlUtils.buildPipelineUrl(this.props.params.organization, pipeline, 'branches');
+        const prUrl = UrlUtils.buildPipelineUrl(this.props.params.organization, pipeline, 'pr');
 
         const pageTabLinks = [
             <Link to={activityUrl}>Activity</Link>,
@@ -147,7 +154,7 @@ export class EnvironmentInfoPage extends React.Component {
         ];
 
         let pipelineRef = {
-            organization: organization,
+            organization: this.props.params.organization,
             pipeline: this.state.pipelineObject,
         };
 
